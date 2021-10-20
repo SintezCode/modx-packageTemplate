23f98ae4227261b55c86efa402bc4dff04387dba
SintezCode <unarmedf@gmail.com>
2021-10-20 17:15:54 +0300

    Add setDir, setName methods.

--- package.php
+++ package.php
@@ -3,5 +3,7 @@
 use SintezCode\MODX\Package;
 
 $package=new Package('package.json');
+$package->setDir('package');
+$package->setName('packageTemplate');
 $package->increaseVersion();
 $package->saveTo('package.json');
